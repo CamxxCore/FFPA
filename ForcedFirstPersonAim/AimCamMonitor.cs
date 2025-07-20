84d96bf0de9e7e5a7902a6c7ff3a85fa5e03be66
Ezio Aditore <xephos374@outlook.com>
2025-07-20 01:09:46 -0600

    Updated to target SHVDN3. Added compatibility for the Enhanced version.

--- ForcedFirstPersonAim/AimCamMonitor.cs
+++ ForcedFirstPersonAim/AimCamMonitor.cs
@@ -22,16 +22,16 @@ namespace ForcedFirstPersonAim
 
         private void OnTick()
         {
-            if (Function.Call<bool>(Hash.IS_CUTSCENE_PLAYING)) return;
+            if (Game.IsCutsceneActive) return;
 
             if (endCam != null && endCam.IsActive && !endCam.IsInterpolating)
                 World.RenderingCamera = null;
 
-            var player = Game.Player.Character;
+            var playerPed = Game.Player.Character;
 
-            if (!player.IsOnFoot ||
-                player.Weapons.Current == null ||
-                Function.Call<int>(Hash.GET_WEAPON_CLIP_SIZE, (int)player.Weapons.Current.Hash) == 0)
+            if (!playerPed.IsOnFoot ||
+                playerPed.Weapons.Current == null ||
+                Function.Call<int>(Hash.GET_WEAPON_CLIP_SIZE, (int)playerPed.Weapons.Current.Hash) == 0)
                 return;
 
             if (Function.Call<bool>(Hash.IS_PLAYER_FREE_AIMING, Game.Player.Handle))
@@ -40,18 +40,18 @@ namespace ForcedFirstPersonAim
                 {
                     var camRot = GameplayCamera.Rotation;
                     var startPos = GameplayCamera.Position;
-                    var endPos = (Game.Player.Character.IsRunning ||
-                        player.IsSprinting) ? 
-                        player.GetOffsetInWorldCoords(new Vector3(-0.009f, 0.07f, 0.60f)) :
-                        player.GetOffsetInWorldCoords(new Vector3(-0.002f, 0.07f, 0.63f));
-
-                    startCam = new Camera(Function.Call<int>(Hash.CREATE_CAM_WITH_PARAMS, "default_scripted_camera", startPos.X, startPos.Y, startPos.Z, camRot.X, camRot.Y, camRot.Z, GameplayCamera.FieldOfView, 0, 2));
-                    endCam = new Camera(Function.Call<int>(Hash.CREATE_CAM_WITH_PARAMS, "default_scripted_camera", endPos.X, endPos.Y, endPos.Z, camRot.X, camRot.Y, camRot.Z, GameplayCamera.FieldOfView, 0, 2));
-
+                    var endPos = (playerPed.IsRunning ||
+                        playerPed.IsSprinting) ? 
+                        playerPed.GetOffsetPosition(new Vector3(-0.009f, 0.07f, 0.60f)) :
+                        playerPed.GetOffsetPosition(new Vector3(-0.002f, 0.07f, 0.63f));
+ 
+                    startCam = new Camera(Function.Call<int>(Hash.CREATE_CAMERA_WITH_PARAMS, 0x019286A9u, startPos.X, startPos.Y, startPos.Z, camRot.X, camRot.Y, camRot.Z, GameplayCamera.FieldOfView, 0, 2));
+                    endCam = new Camera(Function.Call<int>(Hash.CREATE_CAMERA_WITH_PARAMS, 0x019286A9u, endPos.X, endPos.Y, endPos.Z, camRot.X, camRot.Y, camRot.Z, GameplayCamera.FieldOfView, 0, 2));
+  
                     Function.Call(Hash.SET_CAM_ACTIVE_WITH_INTERP, endCam.Handle, startCam.Handle, 180, 0, 1);
                     Function.Call(Hash.RENDER_SCRIPT_CAMS, 1, 0, 3000, 1, 0, 0);
-                    Function.Call(Hash._0x48621C9FCA3EBD28, 4);
-
+                    Function.Call(Hash.REPLAY_START_EVENT, 4);
+                    
                     prevMode = Function.Call<int>(Hash.GET_FOLLOW_PED_CAM_VIEW_MODE);
                     focusEntTimer.Start();
                     hasAimed = true;
@@ -71,8 +71,8 @@ namespace ForcedFirstPersonAim
                 {
                     focusExTimer.Enabled = false;
                     Function.Call(Hash.SET_FOLLOW_PED_CAM_VIEW_MODE, prevMode);
-                    startCam?.Destroy();
-                    endCam?.Destroy();
+                    startCam?.Delete();
+                    endCam?.Delete();
                     World.RenderingCamera = null;
                 }
             }
